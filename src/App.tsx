73ae267f6d65e7136afc35cc49e123919254faf4
İlker Özin <ilker.ozin@selebra.net>
2023-09-04 22:18:55 +0300

    folders etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 import logo from "./assets/bunlarlogo.png";
 import "./assets/App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./themes/defaultTheme";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import MuiAppBar from "@mui/material/AppBar";
+import { Css } from "@mui/icons-material";
+import { CssBaseline } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>Özgürlük, Adalet, Eşitlik</p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <MuiAppBar position="absolute">Bunlar</MuiAppBar>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
 }
 
