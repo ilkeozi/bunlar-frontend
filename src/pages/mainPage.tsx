dafec480898505a476fe029155057fb349cc0888
İlker Özin <ilker.ozin@selebra.net>
2023-09-14 09:36:11 +0300

    added card mainpage

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -2,13 +2,27 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import ReactMarkdown from "react-markdown";
 import data from "./data.json";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
 
 export default function MainPage() {
   return (
     <Container>
       <Grid container spacing={2} padding={2}>
         <Grid item xs={12}>
-          <ReactMarkdown children={data.data}></ReactMarkdown>
+          <Card>
+            <CardHeader
+              title="Türkiye Cumhuriyeti Anayasası"
+              subheader="Kabul Tarihi: 18/10/1982"
+            />
+            <CardContent>
+              <Typography align="justify">
+                <ReactMarkdown children={data.data} />
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </Container>
