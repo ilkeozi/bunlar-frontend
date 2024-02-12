95cf0034d76c2772743e99419b2e34be28e747e0
İlker Özin <ilker.ozin@selebra.net>
2024-02-12 10:12:36 +0300

    added next with pages router and nextui

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders freedom text", () => {
-  render(<App />);
-  const textFreedom = screen.getByText(/Özgürlük, Adalet, Eşitlik/);
-  expect(textFreedom).toBeInTheDocument();
-});
--- src/themes/defaultTheme.tsx
+++
@@ -1,4 +0,0 @@
-import { createTheme, responsiveFontSizes } from "@mui/material/styles";
-
-const theme = createTheme();
-export default responsiveFontSizes(theme);
