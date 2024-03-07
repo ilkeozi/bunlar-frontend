c2c40044cb13fbe5f2d53e92d9db06496454b45f
İlker Özin <ilker.ozin@selebra.net>
2024-03-07 11:18:02 +0300

    added footer

---
+++ src/components/footer-list.tsx
@@ -0,0 +1,54 @@
+export const FooterList = () => {
+  return (
+    <div>
+      <h3 className="text-small font-semibold text-default-600">About Us</h3>
+      <ul className="mt-6 space-y-4">
+        <li>
+          <a
+            className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-small no-underline hover:opacity-80 active:opacity-disabled transition-opacity text-default-400"
+            role="link"
+            href="#"
+          >
+            Our Story
+          </a>
+        </li>
+        <li>
+          <a
+            className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-small no-underline hover:opacity-80 active:opacity-disabled transition-opacity text-default-400"
+            role="link"
+            href="#"
+          >
+            Latest News
+          </a>
+        </li>
+        <li>
+          <a
+            className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-small no-underline hover:opacity-80 active:opacity-disabled transition-opacity text-default-400"
+            role="link"
+            href="#"
+          >
+            Career Opportunities
+          </a>
+        </li>
+        <li>
+          <a
+            className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-small no-underline hover:opacity-80 active:opacity-disabled transition-opacity text-default-400"
+            role="link"
+            href="#"
+          >
+            Media Enquiries
+          </a>
+        </li>
+        <li>
+          <a
+            className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-small no-underline hover:opacity-80 active:opacity-disabled transition-opacity text-default-400"
+            role="link"
+            href="#"
+          >
+            Collaborations
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { title, subtitle } from "../components/primitives";
 import { GithubIcon } from "../components/icons";
 import DefaultLayout from "@/layouts/default";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { FooterList } from "@/components/footer-list";
 
 type Article = {
   title: string;
@@ -28,11 +29,10 @@ export default function IndexPage({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
+      <section className="flex flex-col mx-auto max-w-7xl items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block  text-center justify-center">
           <h1 className={title()}>Make&nbsp;</h1>
           <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-          <br />
           <h1 className={title()}>
             websites regardless of your design experience.
           </h1>
@@ -40,8 +40,7 @@ export default function IndexPage({
             <div key={staticProps[0].title}>{staticProps[0].title}</div>
           </h4>
         </div>
-
-        <div className="flex gap-3">
+        <div className="flex gap-4">
           <Link
             isExternal
             href={siteConfig.links.docs}
@@ -70,6 +69,18 @@ export default function IndexPage({
               <Code color="primary">pages/index.tsx</Code>
             </span>
           </Snippet>
+        </div>
+      </section>
+      <section className="py-8">
+        <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+          <div className="md:grid md:grid-cols-2 md:gap-8">
+            <FooterList />
+            <FooterList />
+          </div>
+          <div className="md:grid md:grid-cols-2 md:gap-8">
+            <FooterList />
+            <FooterList />
+          </div>
         </div>
       </section>
     </DefaultLayout>
