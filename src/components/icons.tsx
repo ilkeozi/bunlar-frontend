1eca9eb9ffdf7ec3a254ef6544f04ae75afd0472
İlker Özin <ilker.ozin@selebra.net>
2024-03-15 11:12:33 +0300

    more mdx formatting

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -245,7 +245,7 @@ export const LoginIcon: React.FC<IconSvgProps> = (props) => (
       stroke-width="1.5"
     >
       <path
-        stroke-linejoin="round"
+        strokeLinejoin="round"
         d="M2.001 11.999h14m0 0l-3.5-3m3.5 3l-3.5 3"
       />
       <path d="M9.002 7c.012-2.175.109-3.353.877-4.121C10.758 2 12.172 2 15 2h1c2.829 0 4.243 0 5.122.879C22 3.757 22 5.172 22 8v8c0 2.828 0 4.243-.878 5.121C20.242 22 18.829 22 16 22h-1c-2.828 0-4.242 0-5.121-.879c-.768-.768-.865-1.946-.877-4.121" />
@@ -313,7 +313,7 @@ export const BunlarLogo: React.FC<IconSvgProps> = (props) => (
       fill="none"
       stroke="currentColor"
       stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
       stroke-width="1.5"
     >
       <path d="M9.533 11.15A1.823 1.823 0 0 0 9 12.438V15h2.578c.483 0 .947-.192 1.289-.534l7.6-7.604a1.822 1.822 0 0 0 0-2.577l-.751-.751a1.822 1.822 0 0 0-2.578 0z" />
--- src/pages/legal/privacy.tsx
+++ src/pages/legal/privacy.tsx
@@ -8,10 +8,6 @@ interface Props {
 }
 
 export default function PrivacyPage({ mdxSource }: Props) {
-  const iconClasses =
-    "text-xl text-default-500 pointer-events-none flex-shrink-0";
-  const text = "Almost before we knew it, we had left the ground.";
-
   return (
     <DefaultLayout>
       <div className="container mx-auto gap-4 py-8 md:py-10">
@@ -20,29 +16,20 @@ export default function PrivacyPage({ mdxSource }: Props) {
           <h4 className={subtitle({ class: "mt-4" })}>Bunlar.org</h4>
         </div>
         <br />
-        <MDXRemote {...mdxSource} />
-        <h1>{text}</h1>
-        <h2>{text}</h2>
-        <h3>{text}</h3>
-        <h4>{text}</h4>
-        <h5>{text}</h5>
-        <h6>{text}</h6>
+        <div className="space-y-4">
+          <MDXRemote {...mdxSource} />
+        </div>
       </div>
     </DefaultLayout>
   );
 }
 
 export async function getStaticProps() {
-  // MDX text - can be from a local file, database, anywhere 65f1f2b65c131fb96c3fc461
   const res = await fetch(
     "https://api-test.bunlar.org/articles/65f1f2b65c131fb96c3fc461"
   );
   const article = await res.json();
-  const multiline = ` 
-  ## Heading
-  Some **mdx** text, with a component
-`;
-  const content = "## Hello World ";
-  const mdxSource = await serialize(multiline);
+
+  const mdxSource = await serialize(article.content);
   return { props: { mdxSource } };
 }
