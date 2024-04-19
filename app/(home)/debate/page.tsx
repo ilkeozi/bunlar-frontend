3f1a4c6be61eaee0b13dddbf865bab4f1a552ca2
İlker Özin <ilker.ozin@selebra.net>
2024-04-19 09:32:52 +0300

    added debate page placeholder

---
+++ app/(home)/debate/page.tsx
@@ -0,0 +1,50 @@
+import { Claim } from "@/components/claim/claim";
+import { subtitle, title } from "@/components/primitives";
+
+export default function DebatePage() {
+  return (
+    <div>
+      <section className="flex flex-col mx-auto max-w-7xl items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block  text-center justify-center">
+          <h1 className={title()}>Make&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>Blog&nbsp;</h1>
+          <h1 className={title()}>
+            websites regardless of your design experience.
+          </h1>
+          <h4 className={subtitle({ class: "mt-4" })}>
+            Beautiful, fast and modern React UI library.
+          </h4>
+        </div>
+      </section>
+      <Claim
+        veracity={5}
+        text={"The use of AI in programming should be welcomed."}
+      />
+
+      <section className="grid grid-cols-2 gap-4 py-8 justify-between ">
+        <div className="flex flex-col gap-4">
+          <Claim
+            veracity={2}
+            text={
+              "AI represents a threat to the foundational principles of democracy."
+            }
+          />
+          <Claim
+            veracity={1}
+            text={
+              "AI has the potential to erode social trust and cause social strife in society."
+            }
+          />
+        </div>
+        <div className="relative ">
+          <Claim
+            veracity={3}
+            text={
+              "AI can enhance the efficiency and transparency of the political decision making process."
+            }
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
